eb8285842d048bb60326b808b6572146e9338ee4
陈宇峰 <yufengcode@gmail.com>
2016-08-11 16:15:56 +0800

    vector的构造函数。

--- STL_vector/main.cpp
+++ STL_vector/main.cpp
@@ -7,9 +7,21 @@
 //
 
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    vector<int> vec1; // 创建一个空的vector
+    vector<int> vec2(vec1); // 创建一个vector vec2,并用vec1去初始化vec2
+    vector<int> vec3(10); // 创建一个含有n个数据的vector
+    vector<int> vec4(10,0); // 创建含有10个数据的vector，并全部初始化为0
+    vec4.~vector(); // 销毁所有数据，释放资源
+
+
+
+
+
     return 0;
 }
